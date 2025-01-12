4b2b9c7831e2b05354dde4fc84f92c0aa318ce36
crutarth <crutarth@gmail.com>
2025-01-12 12:02:16 +0530

    initial commit

---
+++ Condition1.java
@@ -0,0 +1,17 @@
+// write a program to check number is odd or even.
+
+import java.util.*;
+
+public class Condition1 {
+    public static void main(String[] args) {
+        int num;
+        System.out.println("Enter a number: ");
+        Scanner sc = new Scanner(System.in);
+        num = sc.nextInt();
+        if (num % 2 == 0) {
+            System.out.println("Even Number");
+        } else {
+            System.out.println("Odd Number");
+        }
+    }
+}

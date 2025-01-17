2c8782b91bbd4ce1be578fe371e7c89c97efa0f6
crutarth <crutarth@gmail.com>
2025-01-17 22:29:21 +0530

    initial commit

---
+++ Arrays1.java
@@ -0,0 +1,24 @@
+// Lecture: 10.1
+// Arrays
+
+public class Arrays1 {
+    public static void main(String[] args) {
+        int marks[] = {96, 87, 89};
+        for (int i = 0; i < marks.length; i++) {
+            System.out.println(marks[i]);
+        }
+    }
+}
+
+
+// Output:
+// 96
+// 87
+// 89
+
+// Explanation:
+/* 
+    In this program, we have created an array marks and initialized it with values {96, 87, 89}.
+    We have used a for loop to iterate through the array and print the elements of the array.
+    The output of the program will be the elements of the array marks.
+*/
